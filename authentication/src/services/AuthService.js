79b15ce7ef987f88e2847184f4c43a601fdb3ae1
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2023-11-22 11:13:53 -0500

    feat: video not finished yet

---
+++ authentication/src/services/AuthService.js
@@ -0,0 +1,48 @@
+import {ref} from 'vue'
+
+class AuthService {
+    constructor() {
+        this.jwt = ref(''); //json web token
+        this.error = ref(''); //variable en caso ocurra un error en la autenticacion
+    }
+
+    getJwt() {
+        return this.jwt;
+    }
+
+    getError() {
+        return this.error;
+    }
+
+    //Establecer el proceso de auth
+    async login(email, password) {
+        try {
+            //GESTION DE AUTENTICACION
+            const res = await fetch('https://376usy84.directus.app/auth/login', {
+                method: 'POST',
+                headers: { //Aca en los header es donde se incluye la info de autenticacion
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email:email,
+                    password:password
+                })
+            });
+
+            //GESTION DE RESPUESTAS
+            const result = await res.json();
+
+            if ('errors' in result) { //error en proceso de auth
+                this.error = "Login failed"
+                return false;
+            }
+
+            this.jwt = result.data.access_token //.data.access_token -> debido al tipo de respuesta que nos da el servicio
+            return true;
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
