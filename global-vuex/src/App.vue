1ab72110a28152c93b9f470e684af29656bd2031
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-07-02 00:48:11 -0500

    vuex counter example with userStore hook

--- global-vuex/src/App.vue
+++ global-vuex/src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
 import FirstCounterExample from './components/FirstCounterExample.vue';
+import SecondCounterExample from './components/SecondCounterExample.vue'
 </script>
 
 <template>
   <FirstCounterExample />
+  <br>
+  <hr>
+  <br>
+  <SecondCounterExample />
 </template>
---
+++ global-vuex/src/components/SecondCounterExample.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+//store hook
+const store = useStore();
+
+const counter = computed(() => store.state.counter)
+const times2 = computed(() => store.getters.times2)
+const increment = () => store.commit('setCounter', counter.value + 1)
+const decrement = () => store.commit('setCounter', counter.value - 1)
+</script>
+
+<template>
+  <h2>
+    Counter: {{ counter }}
+  </h2>
+
+  <h2>
+    Counter x2 = {{ times2 }}
+  </h2>
+
+  <button 
+    @click="increment"
+  >
+    Add
+  </button>
+
+  <button 
+    @click="decrement"
+  >
+    Reduce
+  </button>
+</template>
+
+<style scoped>
+
+</style>
