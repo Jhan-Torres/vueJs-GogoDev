d1843dab75e0973798b20e0dc7f48e3eea547301
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-07-02 00:11:02 -0500

    set counter example with vuex syntax

--- global-vuex/src/store/index.js
+++ global-vuex/src/store/index.js
@@ -3,17 +3,21 @@ import { createStore } from "vuex";
 export default createStore({
   state: { 
     //todos los datos que va a tener el store
-
+    counter: 1,
   },
   getters: { 
     //métodos para obtener la información de un "state", además de otros valores
     //que se pueden modificar en el flujo (parecido a los "computed")
-
+    times2(state) {
+      return state.counter * 2
+    },
   },
   mutations: { 
     //métodos equivalentes a los setter, para modificar los "states", 
     //solo permiten acciones síncronas.
-
+    setCounter(state, value) {
+      state.counter = value;
+    },
   },
   actions: { 
     //permiten modificar los "states" al igual que las "mutations", pero
@@ -22,6 +26,6 @@ export default createStore({
   },
   modules: { 
     //permiten subdividir el "store" y estructurar a través de sub-elementos
-
+    
   },
 })
