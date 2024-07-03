a81081003fc611cfc6692ca732e693026a209e10
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-07-02 23:48:46 -0500

    pinia with setup stores

--- global-pinia/src/stores/counter.js
+++ global-pinia/src/stores/counter.js
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+//Setup stores syntax
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
@@ -12,7 +13,7 @@ export const useCounterStore = defineStore('counter', () => {
 })
 
 /*
-OPTION API way to use pinia:
+Option Stores syntax
 export const useCounterStore = defineStore('counter', {
   state: () => {
     return {
