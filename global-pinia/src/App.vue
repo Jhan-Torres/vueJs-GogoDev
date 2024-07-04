4bbcc3eaaac711c9c1749ba56fb86f52eff5a36e
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-07-03 22:58:48 -0500

    vueuse library dependences added

--- global-pinia/src/App.vue
+++ global-pinia/src/App.vue
@@ -1,6 +1,33 @@
 <script setup>
+import { computed, ref } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useCounterStore } from './stores/counter';
+
+//vue-use imports
+import { useMouseInElement } from '@vueuse/core'
+
+//Nodes ref
+const cardTarget = ref(null) 
+
+//get variables from vue-use
+const { 
+  elementX, 
+  elementY, 
+  isOutside, 
+  elementHeight, 
+  elementWidth 
+} = useMouseInElement(cardTarget); 
+
+const cardTransformation = computed(() => {
+  const MAX_ROTATION = 6
+  const rotationX = (MAX_ROTATION/2 - (elementY.value/elementHeight.value)*MAX_ROTATION).toFixed(2);
+
+  const rotationY = ((elementX.value/elementWidth.value)*MAX_ROTATION - MAX_ROTATION/2).toFixed(2);
+
+  return isOutside.value 
+    ? ''
+    : `perspective(${elementWidth.value}px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`
+})
 
 const counterStore = useCounterStore();
 
@@ -13,7 +40,7 @@ const msg = aMethod();
 </script>
 
 <template>
-  <div>
+  <div class="card">
     <h1>Hola mundo from PINIA</h1>
     <h2>
       Counter: {{ counterStore.count }}
@@ -35,5 +62,12 @@ const msg = aMethod();
 </template>
 
 <style scoped>
-
+.card {
+  border: 1px solid bisque;
+  padding: 2rem;
+  border-radius: 25px;
+  background-color: #002022;  
+  transform: v-bind(cardTransformation);
+  transition: transform 0.25s ease-out;
+}
 </style>
