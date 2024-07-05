6e9806a272b9065ae89f41024325cfbfd86b298b
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-07-05 17:33:39 -0500

    send values to server

--- route-protect/src/App.vue
+++ route-protect/src/App.vue
@@ -1,85 +1,46 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+let note = ref('')
+
+const sendToServer = async () => {
+  alert("works!")
+  const rawResponse = await fetch('http://', {
+    method: 'POST', 
+    //metodo post ya que este envio solicita informacion y normalmente es POST
+    headers: { 
+    //Aqui es donde definimos que tipo de info estamos enviando
+      'Content-type': 'application/json',  
+      //Tipo de contenido para que el servidor interprete los datos correctamente en JSON
+      'Accept': 'applocation/json', 
+      //(opcional) para indicar el forzado para que acepte el formato application/json
+      'Authorization': 'Bearer YYYYYYYYYYYYYYYYYY', 
+      //(Bearer --> tipo de token) enviamos el JWT para que el servidor sepa que somos usuarios autenticados
+    },
+    body: JSON.stringify({
+      note: note.value,
+    })
+  })
+
+  const res = await rawResponse.json()
+  console.log(res);
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <input 
+      type="text"
+      v-model="note" 
+    />
+    <button
+      @click="sendToServer"
+    >
+      Enviar nota al servidor
+    </button>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
