f4daa79ff71318eef9fa4e9c84b3c176a1c6d2c4
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-07-04 22:13:03 -0500

    using vue sessions

--- using-sessions/src/main.js
+++ using-sessions/src/main.js
@@ -4,8 +4,28 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// after install "npm i vue-session"
+import VueSession from 'vue-session'
+
 const app = createApp(App)
 
 app.use(createPinia())
 
+app.use(VueSession)
+
 app.mount('#app')
+
+/*
+Iniciar una sesion:
+  $session.start()
+Guardar una sesion:
+  $session.set('auth', value)
+Obtener una sesion:
+  $session.get('auth')
+Id de la sesion que estamos usando:
+  $session.id()
+Renovar una sesion:
+  $session.renew()
+Eliminar la sesion:
+  $session.destroy('auth')
+*/
