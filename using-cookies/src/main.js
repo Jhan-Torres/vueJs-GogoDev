29b92f61f2042447b71b80a2a5d8f37deb295bf1
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-07-04 20:15:42 -0500

    using cookies on main file

--- using-cookies/src/main.js
+++ using-cookies/src/main.js
@@ -4,8 +4,20 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// after install "npm i vue-cookies"
+import VueCookies from 'vue-cookies'
+
 const app = createApp(App)
 
 app.use(createPinia())
+
+//configuracion de cookies:
+app.use(VueCookies, {
+  expires: '1d',  //Expires after "1 day" (usual value)
+  // path: '/', //path en donde se van a guardar las cookies
+  // domain: '', //dominio en concreto en donde se va a guardar las cookies
+  // secure: '', //Modo de seguridad que se aplica a las cookies
+  // sameSite: '', //configuraciones sameSite de las cookies
+})
 
 app.mount('#app')
