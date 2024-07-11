cda31ede8405a6358750dd3dffaf6f85a76bc169
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-07-11 17:10:29 -0500

    meta info added on products and cart views

--- auth-example/src/router/index.js
+++ auth-example/src/router/index.js
@@ -7,31 +7,49 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/HomeView.vue'),
+      meta: {
+        requireAuth: false,
+      },
     },
     {
       path: '/products',
       name: 'products',
       component: () => import('../views/ProductsView.vue'),
+      meta: {
+        requireAuth: false,
+      },
     },
     {
       path: '/products/:id',
       name: 'product-detail',
       component: () => import('../views/DetailView.vue'),
+      meta: {
+        requireAuth: true,
+      },
     },
     {
       path: '/cart',
       name: 'cart',
       component: () => import('../views/CartView.vue'),
+      meta: {
+        requireAuth: true,
+      },
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../components/LogInForm.vue')
+      component: () => import('../components/LogInForm.vue'),
+      meta: {
+        requireAuth: false,
+      },
     },
     {
       path: '/signup',
       name: 'signup',
-      component: () => import('../components/SignUpForm.vue')
+      component: () => import('../components/SignUpForm.vue'),
+      meta: {
+        requireAuth: false,
+      }
     }
   ]
 })
