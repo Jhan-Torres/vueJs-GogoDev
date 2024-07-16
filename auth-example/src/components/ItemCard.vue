2ee0af06cdc536c92879ded8138a27b4ed2faa3b
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-07-15 22:40:48 -0500

    product card style modified

--- auth-example/src/components/ItemCard.vue
+++ auth-example/src/components/ItemCard.vue
@@ -18,23 +18,23 @@ defineProps({
         class="object-contain h-72 img-custom-gradient hover:scale-110 duration-200"
       >
     </div>
-    <div class="p-2">
+    <div class="px-2 py-3">
       <div class="flex justify-end">
         <span 
-          class="text-[#f2ece4] bg-[#363234] text-sm px-4 py-0.5 rounded-xl font-semibold"
+          class="text-[#f2ece4] bg-[#363234] px-4 py-0.5 rounded-xl font-semibold"
         >
           {{ item.category }}
         </span>
       </div>
       <h2
-        class="text-[#4A1F43] font-semibold text-lg my-4 truncate"
+        class="text-[#4A1F43] font-bold text-lg my-3 truncate"
       >
         {{ item.title }}
       </h2>
       <div class="flex justify-center">
         <RouterLink
           :to="{ name: 'product-detail', params: { id: item.id } }"
-          class="bg-[#4A1F43] py-1.5 px-20 hover:bg-[#5f2c57] hover:scale-105 duration-150 rounded-md"
+          class="bg-[#4A1F43] py-2 px-20 hover:bg-[#5f2c57] hover:scale-105 duration-150 rounded-md"
         >
           <span 
             class="text-white font-bold tracking-wide"
