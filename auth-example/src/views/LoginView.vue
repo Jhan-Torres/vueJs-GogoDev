cb3f896ba5f56ee6272da8194261b974a0fcbc8e
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-07-18 00:15:56 -0500

    login view bg changed

--- auth-example/src/views/LoginView.vue
+++ auth-example/src/views/LoginView.vue
@@ -3,10 +3,15 @@ import LogInForm from '@/components/LogInForm.vue';
 </script>
 
 <template>
-  <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
-  <LogInForm />
+  <div class="bg-black-and-white w-full h-full">
+    <LogInForm />
+  </div>
 </template>
 
 <style scoped>
-
+.bg-black-and-white {
+  background-image: url('../../public/img/background/black-and-white.svg');
+  background-size: cover;
+  background-attachment: fixed;
+}
 </style>
